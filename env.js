fd11e7a458867c9966f1a6c16eca69b3bfa49c32
Pedro Teles <phtf.pedro12@gmail.com>
2025-04-29 23:08:38 -0300

    somente para ter efeito de .ENV e facilitar nos testes

---
+++ env.js
@@ -0,0 +1,23 @@
+
+/**
+ * Returns the URL of a Markdown file hosted on GitHub.
+ * This function is typically used to retrieve the location of an MD type documentation file.
+ *
+ * @function
+ * @returns {string} The URL of the Markdown file.
+ */
+export function routeFileMD() {
+    return 'https://raw.githubusercontent.com/Plekest/app_document/main/docs.md';
+}
+
+/**
+ * Returns the default programming language used in the environment.
+ * This function is typically used to specify the language context.
+ *
+ * @function
+ * @returns {string} The name of the programming language or markup language ('html', 'javascript').
+ */
+export function language() {
+    return 'javascript';
+}
+
